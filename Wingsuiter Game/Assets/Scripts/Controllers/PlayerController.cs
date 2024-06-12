08d546546cda9e5cb92a331d9b8b8770001f3798
egomm <egomyt@gmail.com>
2024-06-12 12:29:16 +1200

    Adding extras to the movement system by adding flares and making the back of the wingsuiter lift up and down

--- Wingsuiter Game/Assets/Scripts/Controllers/PlayerController.cs
+++ Wingsuiter Game/Assets/Scripts/Controllers/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public GUITextManager textManager;
     public Rigidbody rigidBody;
+    public Utility utility;
 
     // Declare the magnitude for the X movement
     const float xMagnitude = 1;
@@ -23,26 +24,34 @@ public class PlayerController : MonoBehaviour
     {
         float horizontalMovement = Input.GetAxis("Horizontal");
         float verticalMovement = Input.GetAxis("Vertical");
-        if (horizontalMovement != 0)
-        {
-            Debug.Log($"Horizontal axis... {horizontalMovement}");
-        }
-        if (verticalMovement != 0)
-        {
-            Debug.Log($"Vertical axis... {verticalMovement}");
-        }
 
         // Prevent the player from flying backward
+        Debug.Log(verticalMovement);
         if (verticalMovement > 0)
         {
             //transform.Translate(Vector3.forward * Time.deltaTime * verticalMovement * 100;
             // Add the x magnitude to the movement vector
-            //rigidBody.AddForce(100, 0, 0, ForceMode.Impulse);
-            Debug.Log(transform.forward);
+            //rigidBody.AddForce(100, 0, 0, ForceMode.Impulse
             Vector3 forwardDirection = transform.forward;
             forwardDirection.y = 0;
             rigidBody.AddForce(forwardDirection * 100, ForceMode.Impulse);
         }
+
+        // Detect if the player presses the F key (to flare)
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            float magX = Mathf.Sin(utility.ConvertToRadians(transform.rotation.eulerAngles.y));
+            float magZ = Mathf.Cos(utility.ConvertToRadians(transform.rotation.eulerAngles.y));
+            rigidBody.AddForce(new Vector3(magX, 1, magZ) * 5000, ForceMode.Impulse);
+        }
+
+        // Detect if the player moves their tail up/down
+        float xChange = 0;
+        if (Input.GetKey(KeyCode.S) && transform.rotation.eulerAngles.x >= 315 && transform.rotation.eulerAngles.x < 360)
+        {
+            xChange += 2f;
+        }
+
 
         // Update the player position
         //transform.position = transform.position + movement;
@@ -58,7 +67,20 @@ public class PlayerController : MonoBehaviour
         // Get the change in y (depends on the change in z)
         float yChange = zChange * Mathf.Sin(transform.rotation.eulerAngles.z * Mathf.PI / 180) / 2;
 
-        transform.rotation = Quaternion.Euler(-45, transform.rotation.eulerAngles.y + (yChange), transform.rotation.eulerAngles.z + zChange);
+        if (transform.rotation.eulerAngles.z != 90)
+        {
+            yChange += -Mathf.Sin(2 * transform.rotation.eulerAngles.z * Mathf.PI / 180) / 5;
+        }
+
+
+        if (transform.rotation.eulerAngles.x > 315)
+        {
+            xChange -= 1f;
+        }
+
+        Debug.Log(xChange);
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x + xChange, transform.rotation.eulerAngles.y + (yChange), transform.rotation.eulerAngles.z + zChange);
         //-horizontalMovement * Time.deltaTime
 
         // Get the player transform
