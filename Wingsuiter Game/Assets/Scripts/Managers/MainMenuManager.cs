6ab593c2292466dff4d9507d817d122624ac2ae6
egomm <egomyt@gmail.com>
2024-06-19 12:29:04 +1200

    Created main menu + working on menu system

---
+++ Wingsuiter Game/Assets/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // Load the game scene
+        SceneManager.LoadScene(sceneName: "Game");
+    }
+
+    public void QuitGame()
+    {
+        // Quit the game
+        Application.Quit();
+    }
+}
