f4af9c492f582899e818cd1ade8f10bb6940883d
egomm <egomyt@gmail.com>
2024-08-02 10:47:05 +1200

    Adding world creator and world selector

--- Wingsuiter Game/Assets/Scripts/Managers/WorldListManager.cs
+++ Wingsuiter Game/Assets/Scripts/Managers/WorldListManager.cs
@@ -26,24 +26,30 @@ public class WorldListManager : MonoBehaviour
         //{
         Debug.Log("Hello");
         //Instantiate(worldItem, new Vector3(0, 0, 0), Quaternion.identity);
-        for (int i = 0; i < 10; i++)
+        //for (int i = 0; i < 10; i++)
+        Debug.Log(DataManager.worldList.Count);
+        foreach (World world in DataManager.worldList)
         {
-            Button newObj = Instantiate(worldItem);
+            Button worldObj = Instantiate(worldItem);
+
+            // Set the name of the world object
+            worldObj.name = world.worldName;
+
             var paddingObj = Instantiate(padding);
             //newObj.transform.parent = GameObject.Find("Panel").transform;
-            newObj.transform.SetParent(panel.transform);
+            worldObj.transform.SetParent(panel.transform);
             paddingObj.transform.SetParent(panel.transform);
 
-            TextMeshProUGUI[] worldInformation = newObj.GetComponentsInChildren<TextMeshProUGUI>();
+            TextMeshProUGUI[] worldInformation = worldObj.GetComponentsInChildren<TextMeshProUGUI>();
             foreach (TextMeshProUGUI info in worldInformation)
             {
                 if (info.CompareTag("World Title"))
                 {
-                    info.text = $"World {i}";
+                    info.text = world.worldName;
                 }
                 else if (info.CompareTag("Last Opened"))
                 {
-                    info.text = $"Last Opened: {i}";
+                    info.text = $"Last Opened: ";
                 }
             }
         }
