f68827ba604bd98568f24a3cf0dec81102813bf2
egomm <egomyt@gmail.com>
2024-08-16 23:02:40 +1200

    Added a minimap arrow so that is very clear where the user is moving (ie. based on black box testing). Need to clean up the world system (including the player's last saved position)

--- Wingsuiter Game/Assets/Scripts/Controllers/MinimapCameraController.cs
+++ Wingsuiter Game/Assets/Scripts/Controllers/MinimapCameraController.cs
@@ -6,18 +6,15 @@ public class MinimapCameraController : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0, 10000, 0);
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Calculate the offset between the player and the minimap camera
-        //offset = transform.position - player.transform.position;
-    }
+    public GameObject arrow;
 
     // Update is called once per frame
     void Update()
     {
         // Update the position of the minimap camera so that it is always above the player
         transform.position = player.transform.position + offset;
+
+        // Update the arrow rotation
+        arrow.transform.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.y);
     }
 }
